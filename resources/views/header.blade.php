5534626d224f5b39a53292794fc20fa4ac5b692f
Mmaduabuchi <emmanuelukwe1@gmail.com>
2023-08-22 22:22:18 +0100

    second commit with seperate header and footer files

---
+++ resources/views/header.blade.php
@@ -0,0 +1,28 @@
+<div class="col-1"></div>
+<div class="col">
+    <div class="row p-3">
+        <div class="col-6 col-md-2">
+            <h1><span id="sitename">Cy</span>prof</h1>
+        </div>
+        <div class="col-6 d-block d-md-none">
+            {{-- nothing is here yet --}}
+        </div>
+        <div class="col d-none d-md-block p-2">
+            <form action="" method="get">
+                <input type="text" name="searchBox" id="searchBox" placeholder="Search products, brands and categories">
+                <input type="submit" value="SEARCH" class="btnSubmit">
+                {{-- <button type="submit" class="btn btn-primary">SEARCH</button> --}}
+            </form>
+        </div>
+        <div class="col cartContainer p-2">
+            <ul>
+                <li class="list" id="accountListBtn"><span class="fa fa-user"></span>Account <span class="fa fa-chevron-down"></span> </li>
+                <li class="list"><span class="fa fa-question-circle"></span>Help</li>
+                <li class="list"><span class="fa fa-shopping-cart"></span>Cart</li>
+            </ul>
+        </div>
+    </div>
+    {{-- navigation bars section --}}
+    
+</div>
+<div class="col-1"></div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,32 +10,11 @@
         <link rel="stylesheet" href="{{asset("assets/bootstrap-4/css/bootstrap.css")}}">
     </head>
     <body class=" bg-light">
+        {{-- header --}}
         <div class="row">
-            <div class="col-1"></div>
-            <div class="col">
-                <div class="row p-3">
-                    <div class="col-6 col-md-2">
-                        <h1><span id="sitename">Cy</span>prof</h1>
-                    </div>
-                    <div class="col-6 d-block d-md-none">
-                        {{-- nothing is here yet --}}
-                    </div>
-                    <div class="col d-none d-md-block p-2">
-                        <form action="" method="get">
-                            <input type="text" name="searchBox" id="searchBox" placeholder="Search products, brands and categories">
-                            <input type="submit" value="SEARCH" class="btnSubmit">
-                            {{-- <button type="submit" class="btn btn-primary">SEARCH</button> --}}
-                        </form>
-                    </div>
-                    <div class="col cartContainer p-2">
-                        <ul>
-                            <li class="list" id="accountListBtn"><span class="fa fa-user"></span>Account <span class="fa fa-chevron-down"></span> </li>
-                            <li class="list"><span class="fa fa-question-circle"></span>Help</li>
-                            <li class="list"><span class="fa fa-shopping-cart"></span>Cart</li>
-                        </ul>
-                    </div>
-                </div>
-                {{-- navigation bars section --}}
+            @include('header')
+        </div>
+        {{-- end for header --}}
         <div class="row">
             <div class="col">
                 <div class="navigationContainer">
@@ -56,13 +35,11 @@
             <div class="col"></div>
             <div class="col"></div>
         </div>
-            </div>
-            <div class="col-1"></div>
-        </div>
-
+        {{-- footer --}}
         <div class="row">
             @extends('footer')
         </div>
+        {{-- end for footer --}}
         <script src="{{asset("assets/bootstrap-4/js/bootstrap.js")}}"></script>
     </body>
 </html>
